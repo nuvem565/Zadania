5670a5b3eb0c14f6864c4abe188b8ad69f64118f
nuvem565 <tom.jedrzejewski565@gmail.com>
2021-02-11 19:44:22 +0100

    Char buffers for processing data, decision - input char for console program control

--- zadanie2/main.c
+++ zadanie2/main.c
@@ -7,3 +7,10 @@ void decimalToBinary(uint32_t decimal, char * outBuff);
 int binaryToDecimal(char * binary);
 
 
+int main()
+{
+    int number = 0;
+    char decision;
+    char charBuff[33], outBuff[33];
+
+
